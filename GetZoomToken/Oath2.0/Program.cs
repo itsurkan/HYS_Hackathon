32a59628fc5df38530603d086cfd90df12b95de2
Roman Bychkov <roman.bychkov.1921@gmail.com>
2024-09-06 19:40:54 +0300

    added server

--- GetZoomToken/Oath2.0/Program.cs
+++ GetZoomToken/Oath2.0/Program.cs
@@ -2,13 +2,13 @@
 
 class Program
 {
-    private static readonly string clientId = "1";
-    private static readonly string clientSecret = "2";
+    private static readonly string clientId = "3upUbFDdTlW_fBvH3eQXsQ";
+    private static readonly string clientSecret = "yzDuR8Cp6sFz32khdKruAqP4kNbno0vy";
     private static readonly string redirectUri = "http://localhost:5000/";
 
     static async Task Main(string[] args)
     {
-        var authorizationCode = "AUTHORIZATION_CODE"; 
+        var authorizationCode = "heyIq76dO77Or_L6-61QVu8ypdVNu544g";
 
         var authHeaderValue = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{clientId}:{clientSecret}"));
 
@@ -16,11 +16,14 @@ class Program
         {
             client.DefaultRequestHeaders.Add("Authorization", $"Basic {authHeaderValue}");
 
-            var content = new StringContent($"grant_type=authorization_code&code={authorizationCode}&redirect_uri={redirectUri}", Encoding.UTF8, "application/x-www-form-urlencoded");
+            var content = new StringContent(
+                $"grant_type=authorization_code&code={authorizationCode}&redirect_uri={redirectUri}",
+                Encoding.UTF8, "application/x-www-form-urlencoded");
 
             var response = await client.PostAsync("https://zoom.us/oauth/token", content);
             var responseContent = await response.Content.ReadAsStringAsync();
 
+            Console.WriteLine("Response:");
             Console.WriteLine(responseContent);
         }
     }
---
+++ GetZoomToken/Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var listener = new HttpListener();
+        var redirectUri = "http://localhost:5000/";
+        listener.Prefixes.Add(redirectUri);
+
+        listener.Start();
+        Console.WriteLine($"Ожидаем редирект на {redirectUri}...");
+
+        // Ожидаем запрос с кодом авторизации
+        var context = await listener.GetContextAsync();
+        var code = context.Request.QueryString["code"];
+
+        Console.WriteLine($"Получен код авторизации: {code}");
+
+        // Отправляем ответ в браузер
+        var response = context.Response;
+        var responseString = "<html><body>Авторизация прошла успешно. Можете вернуться в приложение.</body></html>";
+        var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+        response.ContentLength64 = buffer.Length;
+        var output = response.OutputStream;
+        await output.WriteAsync(buffer, 0, buffer.Length);
+        output.Close();
+
+        listener.Stop();
+
+        // Далее используем полученный код для обмена на access_token
+    }
+}
