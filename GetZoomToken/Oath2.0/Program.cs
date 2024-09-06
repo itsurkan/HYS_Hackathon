4d2fd0fb14dc1d5471db8de4bb1694621b55c2ef
Roman Bychkov <roman.bychkov.1921@gmail.com>
2024-09-06 18:28:57 +0300

    example

---
+++ GetZoomToken/Oath2.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+class Program
+{
+    private static readonly string clientId = "1";
+    private static readonly string clientSecret = "2";
+    private static readonly string redirectUri = "http://localhost:5000/";
+
+    static async Task Main(string[] args)
+    {
+        var authorizationCode = "AUTHORIZATION_CODE"; 
+
+        var authHeaderValue = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{clientId}:{clientSecret}"));
+
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("Authorization", $"Basic {authHeaderValue}");
+
+            var content = new StringContent($"grant_type=authorization_code&code={authorizationCode}&redirect_uri={redirectUri}", Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            var response = await client.PostAsync("https://zoom.us/oauth/token", content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseContent);
+        }
+    }
+}
