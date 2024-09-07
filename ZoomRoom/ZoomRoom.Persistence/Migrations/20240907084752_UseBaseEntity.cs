cfeb67b84017765300f5a0c28ffe21993bbbff86
Ivan Tsurkan <itsurkan@taskmaverick.com>
2024-09-07 11:48:54 +0300

    add migration

---
+++ ZoomRoom/ZoomRoom.Persistence/Migrations/20240907084752_UseBaseEntity.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZoomRoom.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoomUsers_Rooms_RoomId",
+                table: "RoomUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "RoomId",
+                table: "RoomUsers",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "RoomUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "RoomUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Rooms",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Rooms",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Meetings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Meetings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoomUsers_Rooms_Id",
+                table: "RoomUsers",
+                column: "Id",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoomUsers_Rooms_Id",
+                table: "RoomUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "RoomUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "RoomUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Meetings");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "RoomUsers",
+                newName: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoomUsers_Rooms_RoomId",
+                table: "RoomUsers",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
